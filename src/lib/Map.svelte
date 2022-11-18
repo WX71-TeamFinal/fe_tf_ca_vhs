6d45059fa693776dab09beec5c61e04c49078147
adrianVelasco <64821523+Luadveand@users.noreply.github.com>
2022-11-18 16:35:04 -0500

    Initial commit

---
+++ src/lib/Map.svelte
@@ -0,0 +1,15 @@
+<script>
+  const fetchImage = (async () => {
+    const response = await fetch("http://127.0.0.1:5000/generated_map");
+    console.log(response);
+    return await response;
+  })();
+</script>
+
+{#await fetchImage}
+  <p>...waiting</p>
+{:then data}
+  <img src={data.message} alt="Dog image" />
+{:catch error}
+  <p>An error occurred!</p>
+{/await}
